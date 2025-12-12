b900225dd7ed4eecec7adbaf241c9503b3494fdf
jonmgerman <68759869+jonmgerman@users.noreply.github.com>
2025-12-11 20:36:02 -0500

    Update Cognito configuration with hardcoded values

--- birdtracker/js/signin.js
+++ birdtracker/js/signin.js
@@ -1,13 +1,13 @@
 // ---------------------------
 // Cognito Configuration
 // ---------------------------
-
-const cognitoDomain = config.auth.authority_uri;
-const clientId = config.auth.client_id;
-const redirectUri = config.auth.redirectUri; //window.location.origin + window.location.pathname;
+const cognitoDomain = 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_3WiAnFAoi';
+const clientId = '1a4nhn6ofmmkj8krvnci85dssk';
+const redirectUri = "https://jonmgerman.github.io/birdtracker/home.html"; //window.location.origin + window.location.pathname;
+const signInUri = 'https://us-east-13wianfaoi.auth.us-east-1.amazoncognito.com/oauth2/authorize';
 const responseType = "code"; // or "code" for auth code flow
 const scope = "email+openid+profile"
-const code_challenge_method = "S256";
+const challengeMethod = "S256";
 let accessToken = null;
 
 // ---------------------------
@@ -71,13 +71,12 @@ signInBtn.onclick = async () => {
     // window.location.href = `${cognitoDomain}/login?client_id=${clientId}&response_type=${responseType}&scope=${scope}&redirect_uri=${encodeURIComponent(redirectUri)}&code_challenge=${codeChallenge}&code_challenge_method=${code_challenge_method}&state=${state}`;
     console.log('cod verifier:', codeVerifier);
         // Build the full Cognito login URL
-    const authUrl = `${config.auth.sign_in_uri}` +
-      `?client_id=${encodeURIComponent(config.auth.client_id)}` +
-      `&response_type=${encodeURIComponent(config.auth.response_type)}` +
-      `&scope=${encodeURIComponent(config.auth.scope)}` +
-      `&redirect_uri=${encodeURIComponent(config.auth.redirectUri)}` +
+    const authUrl = `${signInUri}` +
+      `?client_id=${encodeURIComponent(clientId)}` +
+      `&response_type=${encodeURIComponent(responseType)}` +
+      `&redirect_uri=${encodeURIComponent(redirectUri)}` +
       `&code_challenge=${encodeURIComponent(codeChallenge)}` +
-      `&code_challenge_method=${encodeURIComponent(config.auth.code_challenge_method) || 'S256'}` +
+      `&code_challenge_method=${encodeURIComponent(challengeMethod) || 'S256'}` +
       `&state=${encodeURIComponent(state)}`;
 
     // Redirect the browser to Cognito Hosted UI
